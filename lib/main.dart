4a335c8318e80511e3860f41011b83fde65b414d
John Kim <jkdoodle1113@gmail.com>
2024-05-09 15:35:28 +0100

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Astral',
+      title: 'Astr-al',
       theme: ThemeData(
         // This is the theme of your application.
         //
