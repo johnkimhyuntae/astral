82c7aff9ab3d1c5fde25f7c22ca185ee3505ff71
Advay <advay168@gmail.com>
2024-05-15 00:06:00 +0100

    Changes

--- lib/day_page.dart
+++ lib/day_page.dart
@@ -12,43 +12,47 @@ class DayPage extends StatelessWidget {
 
   final DateTime date;
 
-  String dateString(){
+  String dateString() {
     return DateFormat("EEEE, d MMMM y").format(date);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Image.asset(
-            "assets/images/moon.png",
-            width: 300,
+          child: SizedBox(
+            height: 130,
+            child: Image.asset(
+              "assets/images/moon.png",
+            ),
           ),
         ),
-        Text(
-          dateString(),
-          style: const TextStyle(color: Colors.white),
-        ),
-        Flexible(
-          child: GridView.count(
-            padding: const EdgeInsets.all(20),
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            children: const <Widget>[
-              WeatherInfoCard(title: "Wind Speed"),
-              WeatherInfoCard(title: "bar"),
-              WeatherInfoCard(title: "baz"),
-              WeatherInfoCard(title: "foo"),
-              WeatherInfoCard(title: "bar"),
-              WeatherInfoCard(title: "baz"),
-              WeatherInfoCard(title: "foo"),
-              WeatherInfoCard(title: "bar"),
-              WeatherInfoCard(title: "baz"),
-            ],
+        Center(
+          child: Text(
+            dateString(),
+            style: const TextStyle(color: Colors.white, fontSize: 28),
           ),
+        ),
+        GridView.count(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          padding: const EdgeInsets.all(20),
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: const <Widget>[
+            WeatherInfoCard(title: "Wind Speed"),
+            WeatherInfoCard(title: "bar"),
+            WeatherInfoCard(title: "baz"),
+            WeatherInfoCard(title: "foo"),
+            WeatherInfoCard(title: "bar"),
+            WeatherInfoCard(title: "baz"),
+            WeatherInfoCard(title: "foo"),
+            WeatherInfoCard(title: "bar"),
+            WeatherInfoCard(title: "baz"),
+          ],
         ),
       ],
     );
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,33 +4,100 @@ import 'package:flutter/widgets.dart';
 import 'day_page.dart';
 import 'log_page.dart';
 
-const IconData collectionsBookmarkRounded =
-    IconData(0xf656, fontFamily: 'MaterialIcons');
+const Color topBg = Color.fromRGBO(253, 245, 230, 1.0);
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  void changeLocation(String s) {}
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Astral"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const TopBar(),
+            Flexible(child: DayPage(date: DateTime.now())),
+          ],
+        ),
       ),
-      body: DayPage(date: DateTime.now()),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const LogPage()),
-          );
-        },
-        child: const Icon(collectionsBookmarkRounded),
+    );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  const TopBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 15.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LogPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    backgroundColor: topBg,
+                    shape: const CircleBorder()),
+                child: const Icon(Icons.format_list_bulleted, size: 30)),
+            const LocationField(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(10),
+                backgroundColor: topBg,
+                shape: const CircleBorder(),
+              ),
+              child: const Icon(
+                Icons.edit,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LocationField extends StatelessWidget {
+  const LocationField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextFormField(
+        initialValue: "Cambridge, UK",
+        onFieldSubmitted: (String location) {},
+        textInputAction: TextInputAction.search,
+        style: const TextStyle(),
+        decoration: InputDecoration(
+          isDense: true,
+          filled: true,
+          fillColor: topBg,
+          hintText: "Location",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: const BorderSide(
+              style: BorderStyle.none,
+            ),
+          ),
+          suffixIcon: const Icon(Icons.search, size: 30),
+        ),
       ),
     );
   }
