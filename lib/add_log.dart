688531ada52604d52bab375e684fe91a82c71094
John Kim <jkdoodle1113@gmail.com>
2024-05-16 20:15:34 +0100

    BIG CHANGE TO LOG ADDING SYSTEM AND ASTRALSTATE

--- lib/add_log.dart
+++ lib/add_log.dart
@@ -3,12 +3,16 @@ import 'package:astral/log_entry_expanded_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'astral_state.dart';
+import 'log_data.dart';
+import 'package:uuid/uuid.dart';
+import 'log_page.dart';
 
 const Color topBg = Color.fromRGBO(253, 245, 230, 1.0);
 
 class AddLog extends StatefulWidget {
-  final DateTime date;
-  const AddLog({super.key, required this.date});
+  const AddLog({super.key});
   
   @override
   State<AddLog> createState() => _AddLogState();
@@ -18,10 +22,13 @@ class _AddLogState extends State<AddLog> {
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<AstralState>();
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     String userTitle = '';
-    String userDescription = '';
+    String userLog = '';
+    var uuid = Uuid();
+    DateTime date = appState.currentDateTime;
 
     return Center(
       child: SizedBox(
@@ -59,7 +66,7 @@ class _AddLogState extends State<AddLog> {
                       padding: const EdgeInsets.only(left: 12.0, bottom: 16.0),
                       child: Align(
                         alignment: Alignment.topLeft,
-                        child: Text(DateFormat("EEEE, d MMMM y").format(widget.date),
+                        child: Text(DateFormat("EEEE, d MMMM y").format(date),
                             style: TextStyle(
                                 color: Theme.of(context).colorScheme.primary
                             ),
@@ -91,7 +98,7 @@ class _AddLogState extends State<AddLog> {
                             onChanged: (value) {
                               // This function is called whenever the user types in the text field
                               setState(() {
-                                userDescription = value;
+                                userLog = value;
                               });
                             },
                             decoration: const InputDecoration(
@@ -114,7 +121,31 @@ class _AddLogState extends State<AddLog> {
                         color: Colors.red,
                         icon: const Icon(Icons.close),
                         onPressed: () {
-                          Navigator.pop(context);
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder: (_, __, ___) => LogPage(),
+                              transitionDuration: Duration.zero,
+                              reverseTransitionDuration: Duration.zero,
+                            ),
+                          );
+                        },
+                        )
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 7.0, top: 20.0),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: IconButton(
+                        iconSize: 35,
+                        color: Colors.red,
+                        icon: const Icon(Icons.add),
+                        onPressed: () {
+                          LogData x = LogData(id: uuid.v4(), title: userTitle, date: date, log: userLog, pathToImage: "assets/images/moon.png");
+                          appState.logs.add(x);
+                          print(appState.logs);
+
                         },
                         )
                       ),
--- lib/astral_state.dart
+++ lib/astral_state.dart
@@ -1,17 +1,21 @@
 import 'dart:math';
 
+import 'package:astral/log_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
 
 class AstralState extends ChangeNotifier {
   ScrollController? controller;
   DateTime currentDateTime;
   Map<String, String> statIdTitleMap;
   Map<String, String> statIdValueMap;
+  List<LogData> logs;
 
   AstralState()
       : currentDateTime = DateTime.now(),
+        logs = [],
         statIdTitleMap = {
           "wind-speed": "Wind Speed",
           "wind-direction": "Wind Direction",
@@ -31,5 +35,19 @@ class AstralState extends ChangeNotifier {
     // TODO: API Call
     statIdValueMap["wind-speed"] = "${Random().nextInt(30)}";
     notifyListeners();
+  }
+  void addNewLog(LogData x) {
+    for (LogData y in logs) {
+      if (x.id == y.id) {
+        return;
+      }
+      else {
+        logs.add(x);
+      }
+    }
+  }
+  void removeLog(LogData x) {
+    logs.remove(x);
+    return;
   }
 }
