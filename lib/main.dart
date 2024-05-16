a02a45ba304a8230586fed83ef464b4e6ef1993c
Advay <advay168@gmail.com>
2024-05-16 10:18:15 +0100

    Pull state upward

--- lib/main.dart
+++ lib/main.dart
@@ -16,31 +16,30 @@ class AstralApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const foreground = Color.fromRGBO(243, 229, 165, 1.0);
     const background = Color.fromRGBO(37, 40, 58, 1.0);
-    return MaterialApp(
-      title: 'Astral',
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: const ColorScheme(
-            // TODO: Change each color to semantically correct thing
-            brightness: Brightness.dark,
-            primary: Colors.black,
-            onPrimary: foreground,
-            secondary: Colors.black,
-            onSecondary: foreground,
-            error: Colors.black,
-            onError: foreground,
-            background: background,
-            onBackground: Colors.white,
-            surface: foreground,
-            onSurface: Colors.black),
+    return ChangeNotifierProvider(
+      create: (context) => AstralState(),
+      child: MaterialApp(
+        title: 'Astral',
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: const ColorScheme(
+              // TODO: Change each color to semantically correct thing
+              brightness: Brightness.dark,
+              primary: Colors.black,
+              onPrimary: foreground,
+              secondary: Colors.black,
+              onSecondary: foreground,
+              error: Colors.black,
+              onError: foreground,
+              background: background,
+              onBackground: Colors.white,
+              surface: foreground,
+              onSurface: Colors.black),
+        ),
+        themeMode: ThemeMode.dark,
+        home: const HomePage(),
+        debugShowCheckedModeBanner: false,
       ),
-      themeMode: ThemeMode.dark,
-      home: ChangeNotifierProvider(
-        create: (context) => AstralState(),
-        child: const HomePage(),
-      ),
-
-      debugShowCheckedModeBanner: false,
     );
   }
 }
