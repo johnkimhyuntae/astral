42cb19bca5e807a3e8b9fc84a068892cd2c095bd
njgal <nj.galvez@yahoo.co.uk>
2024-05-18 17:08:36 +0100

    API handler file

--- lib/astral_state.dart
+++ lib/astral_state.dart
@@ -5,10 +5,15 @@ import 'time_weather_info_card.dart';
 import 'weather_info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:reorderable_staggered_scroll_view/reorderable_staggered_scroll_view.dart';
+import 'API_handler.dart';
 
 class AstralState extends ChangeNotifier {
   ScrollController controller = ScrollController();
   DateTime latestUpdatedTime;
+
+  double lat = 52.1951;
+  double lon = 0.1313;
+
   Map<String, String> statIdTitleMap = {
     "wind-speed": "Wind Speed",
     "wind-direction": "Wind Direction",
@@ -32,9 +37,33 @@ class AstralState extends ChangeNotifier {
   }
 
   void updateWeather() {
-    // TODO: API Call
-    statIdValueMap["wind-speed"] = "${Random().nextInt(30)}";
-    notifyListeners();
+    API_caller API_instance = API_caller();
+    Future<Album> futureAlbum = API_instance.callAPI(lat, lon);
+    futureAlbum.then((weatherAlbum) {
+      statIdValueMap["time"] = weatherAlbum.time[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+      statIdValueMap["wind-speed"] = weatherAlbum.windspeed[latestUpdatedTime.hour].toString();
+
+      notifyListeners();
+    });
+
+
+    //statIdValueMap["wind-speed"] = "${Random().nextInt(30)}";
+    //notifyListeners();
+  }
+
+  void updateLocation(String newLocation) {
+    Future<List<double>> futureLatLon = API_handler.fetchLatLon(newLocation);
+    futureLatLon.then((latLon) {
+      lat = latLon[0];
+      lon = latLon[1];
+      updateWeather();
+    });
   }
 
   void addNewLog(LogData x) {
